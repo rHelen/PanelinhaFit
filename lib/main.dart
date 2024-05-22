d243f8ed3daad9028cc6af70a6bfa279ca97dd7e
Rute Helen <rutehelenrh@gmail.com>
2024-05-22 13:36:36 -0300

    Segunda tela corrigida

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(LoginApp());
@@ -80,7 +81,7 @@ class _LoginPageState extends State<LoginPage> {
                 if(email=='email.com' && password=='123'){
                    Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => SegundaTela()),
+                    MaterialPageRoute(builder: (context) => SegundaTela2()),
                   );
                 }
                 _login();
@@ -119,78 +120,132 @@ class _LoginPageState extends State<LoginPage> {
   }
 }
 
-class SegundaTela extends StatelessWidget {
+class SegundaTela2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('../assets/backHomeApp.png'), // Caminho para sua imagem
-              fit: BoxFit.cover, // Ajusta a imagem para cobrir todo o container
+      backgroundColor: Color(0xFF040116), // cor de fundo azul
+      appBar: AppBar(
+        title: Text('Minha Aplicação'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color(0xFF040116),
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // implementação para o item 1
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // implementação para o item 2
+              },
+            ),
+            // Adicione mais itens do menu conforme necessário
+          ],
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          // Adiciona a imagem na parte superior da tela
+          Image.asset(
+            'assets/logopanelinha.png',
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover,
+          ),
+          // Conteúdo restante
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Text(
+                  'Texto acima das imagens',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        CircleAvatar(
+                          backgroundImage: AssetImage('assets/image1.png'),
+                          radius: 40,
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'texto abaixo de uma imagem',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    CircleAvatar(
+                      backgroundImage: AssetImage('assets/image2.png'),
+                      radius: 40,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20), // Espaçamento entre as imagens e a parte inferior da tela
+              ],
             ),
           ),
-          child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'PANELINHA',
-              style: TextStyle(
-                color: Colors.white, fontSize: 24.0,
-              ),
-            ),
-            Text(
-              'Fit',
-              style: TextStyle(
-                color: Colors.white, // Definindo a cor azul para a fonte
-              ),
-            ),
-          ]
-        ),
+        ],
       ),
     );
   }
 }
 
-// class HomePage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context){
-//     return MaterialApp(
-//       title: 'Home Page',
-//       home: HomePage(),
-//     );
-//   }
-// }
+class MinhaTela extends StatefulWidget {
+  @override
+  _MinhaTelaState createState() => _MinhaTelaState();
+}
 
-// class _HomePage extends StatelessWidget{
-//   @override
-//   Widget build(BuildContext context){
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Home Page'),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Text(
-//               'Bem-vindo à Página Inicial',
-//               style: TextStyle(
-//                 fontSize: 24,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//             SizedBox(height: 20),
-//             ElevatedButton(
-//               onPressed: (){
-//                 //Chamar tela de treino TreinoPage();
-//               }, 
-//               child: Text('Botão'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     )
-//   }
-// }
+class _MinhaTelaState extends State<MinhaTela> {
+  TextEditingController nomeUser = TextEditingController();
+  
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('../assets/sawako.jpg'),
+          fit: BoxFit.cover,
+        )
+      ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+    ),
+      ),
+    );
+  }
+}
